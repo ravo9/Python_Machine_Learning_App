b73a92c29b52943cf8c449572a5c9654344d5b56
Rafal Ozog <rafalozog@gmail.com>
2022-03-20 04:10:07 +0000

    Code cleaning

---
+++ prepare_models/make_models/main.py
@@ -0,0 +1,22 @@
+from multiple_models_creation_manager import start_creation_of_machine_learning_models
+from config_machine_learning_parameters import get_config_machine_learning_parameters_many
+from config_data_parameters import get_config_data_parameters_many
+from config_multiple_models_creation_process_parameters import get_config_multiple_models_creation_process_parameters
+
+# How to update the dates:
+# The start_test_date doesn't have to be next day after end_train_date. It may be
+# set as day 'next day after end_train_date' - days_into_account + 1 .
+
+# Todo 1. Automate setting testing period.
+
+
+# Main function
+config_data_parameters = get_config_data_parameters_many()
+config_machine_learning_parameters = get_config_machine_learning_parameters_many()
+config_multiple_models_creation_process_parameters = get_config_multiple_models_creation_process_parameters()
+
+start_creation_of_machine_learning_models(
+    config_data_parameters,
+    config_machine_learning_parameters,
+    config_multiple_models_creation_process_parameters
+)
--- prepare_models/make_models/multiple_models_creation_manager.py
+++ prepare_models/make_models/multiple_models_creation_manager.py
@@ -1,23 +1,14 @@
 import os
 import time
 from utils_csv_and_txt import write_opening_separator_into_txt_log, write_closing_separator_into_txt_log
-from model_creator import make_multiple_variable_model
+from single_model_creator import make_multiple_variable_model
 from utils import print_currently_making_model_info, print_model_creation_interrupted_error
-from config_machine_learning_parameters import get_config_machine_learning_parameters_many
-from config_data_parameters import get_config_data_parameters_many
-from config_multiple_models_creation_process_parameters import get_config_multiple_models_creation_process_parameters
-
-# How to update the dates:
-# The start_test_date doesn't have to be next day after end_train_date. It may be
-# set as day 'next day after end_train_date' - days_into_account + 1 .
-
-# Todo 1. Automate setting testing period.
 
 
-def start_creation_of_machine_learning_models():
-    config_data_parameters = get_config_data_parameters_many()
-    config_machine_learning_parameters = get_config_machine_learning_parameters_many()
-    config_multiple_models_creation_process_parameters = get_config_multiple_models_creation_process_parameters()
+def start_creation_of_machine_learning_models(
+    config_data_parameters,
+    config_machine_learning_parameters,
+    config_multiple_models_creation_process_parameters):
 
     output_dir = config_multiple_models_creation_process_parameters["output_dir"]
     amount_of_models_per_one_setting = config_multiple_models_creation_process_parameters["amount_of_models_per_one_setting"]
@@ -49,7 +40,3 @@ def start_creation_of_machine_learning_models():
     # Run the whole script as sudo to let it work.
     if sleep_the_computer_when_the_work_is_done:
         os.system('shutdown -s now')
-
-
-# Main function
-start_creation_of_machine_learning_models()
