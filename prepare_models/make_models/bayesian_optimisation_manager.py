11088e4333eb76b4099439c0e9d1b812cba7f405
Rafal Ozog <rafalozog@gmail.com>
2022-06-05 11:51:16 +0100

    Code update

--- prepare_models/make_models/bayesian_optimisation_manager.py
+++ prepare_models/make_models/bayesian_optimisation_manager.py
@@ -5,12 +5,12 @@ def start_bayesian_optimisation_analysis():
 
     # Set paramaters
     params_nn ={
-        # 'days_into_account':(5, 45),
-        # 'epochs_amount':(2, 30),
+        'days_into_account':(3, 45),
+        'epochs_amount':(2, 30),
         # 'batch_size':(1, 20),
-        # 'layer_1_neurones_number':(0, 150),
-        # 'layer_2_neurones_number':(0, 150),
-        # 'layer_3_neurones_number':(0, 150),
+        'layer_1_neurones_number':(0, 150),
+        'layer_2_neurones_number':(0, 150),
+        'layer_3_neurones_number':(0, 150)
         # 'optimizer_learning_rate':(0.0001, 0.01)
     }
 
@@ -23,28 +23,27 @@ def start_bayesian_optimisation_analysis():
 
 
 def runModelMaking(
-    # layer_1_neurones_number,
-    # layer_2_neurones_number,
-    # layer_3_neurones_number,
-    # days_into_account,
-    # epochs_amount,
+    days_into_account,
+    epochs_amount,
     # batch_size,
+    layer_1_neurones_number,
+    layer_2_neurones_number,
+    layer_3_neurones_number
     # optimizer_learning_rate
     ):
 
     # Prepare config_data_parameters
     columns = ['Close']
     start_train_date='2015-09-15'
-    end_train_date='2021-07-24'
-    start_test_date='2021-06-27'
-    end_test_date='2021-11-25'
-    instrument = 'AAPL'
+    start_test_date='2022-01-01'
+    end_test_date='2022-06-01'
+    instruments = ['NDX', 'CL=F']
 
     # Prepare config_machine_learning_parameters
     optimizer_type = 'adam'
     loss_function_type = 'mean_absolute_error'
-    days_into_account = 10
-    epochs_amount = 6
+    # days_into_account = 10
+    # epochs_amount = 6
     batch_size = 1
     random_seed = 2323
     optimizer_learning_rate = 0.0009
@@ -53,24 +52,23 @@ def runModelMaking(
     output_dir = 'models_creation_output/'
     average_required_for_model_to_be_saved = 0.0060
 
-    # epochs_amount = int(epochs_amount)
+    epochs_amount = int(epochs_amount)
     # batch_size = int(batch_size)
-    # days_into_account = int(days_into_account)
-    # layer_1_neurones_number = int(layer_1_neurones_number)
-    # layer_2_neurones_number = int(layer_2_neurones_number)
-    # layer_3_neurones_number = int(layer_3_neurones_number)
+    days_into_account = int(days_into_account)
+    layer_1_neurones_number = int(layer_1_neurones_number)
+    layer_2_neurones_number = int(layer_2_neurones_number)
+    layer_3_neurones_number = int(layer_3_neurones_number)
 
-    layer_1_neurones_number = 18
-    layer_2_neurones_number = 99
-    layer_3_neurones_number = 37
+    # layer_1_neurones_number = 18
+    # layer_2_neurones_number = 99
+    # layer_3_neurones_number = 37
 
     score = make_multiple_variable_model(
         columns,
         start_train_date,
-        end_train_date,
         start_test_date,
         end_test_date,
-        instrument,
+        instruments,
         optimizer_type,
         loss_function_type,
         days_into_account,
